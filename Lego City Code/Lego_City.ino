6fccbd9ae12b06cdb83e5666eca059f0c93830c0
Vincent Hopf - Jidi Parts <51283084+Jidiparts@users.noreply.github.com>
2020-04-05 11:58:14 +1000

    Update Lego_City.ino

--- Lego City Code/Lego_City.ino
+++ Lego City Code/Lego_City.ino
@@ -24,7 +24,7 @@ void loop()
     delay(2000);
     oled_1.clearDisplay(); oled_1.display();
     oled_1.drawRect(0,0,128,64,WHITE); oled_1.display();
-    oled_1.setTextSize(2); oled_1.setTextColor(WHITE); oled_1.setCursor(7,7); oled_1.print(String("SUNDAY     5/4/2020  11:57 pm")); oled_1.display();
+    oled_1.setTextSize(2); oled_1.setTextColor(WHITE); oled_1.setCursor(7,7); oled_1.print(String("SUNDAY     5/4/2020  11:57 am")); oled_1.display();
     delay(2000);
     
 }
